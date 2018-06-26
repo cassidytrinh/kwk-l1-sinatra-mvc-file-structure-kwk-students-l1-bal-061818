4fcd38e5f5b192f68820cf3aa91c7f1987cbbfe3
cassidytrinh <cassidytrinh@gmail.com>
2018-06-26 14:19:25 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,12 @@
-class Model
-  # Replace with Dog clas
+class Dog
+  attr_accessor :name, :breed, :age
+  @@count = 0
+  @@dogs = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@count += 1
+  end
 end
